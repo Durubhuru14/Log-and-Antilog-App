179099b39f5f1d4f016f8c4e598a3f50a8ee8f37
Durvesh More <durubhuru01@gmail.com>
2025-02-18 21:54:03 +0530

    Refactored the logic to be more concise and reusable

--- log and antilog.js
+++ log and antilog.js
@@ -1,156 +1,164 @@
-// When I wrote this, only God and I understood what I was doing
-// Now, God only knows
-// I ain't refactoring this pile of shit
-
+// Helper function to convert a float to a fixed decimal format
 function decimalConverter(floatNum) {
-  if (floatNum < 1) {
-    if (floatNum < 1.000 && floatNum >= 0.1) {
-      floatNum *= 10
-    } else if (floatNum < 0.1 && floatNum >= 0.01) {
-      floatNum *= 100
-    } else if (floatNum < 0.01 && floatNum >= 0.001) {
-      floatNum *= 1000
-    } else if (floatNum < 0.001 && floatNum >= 0.0001) {
-      floatNum *= 10000
+    if (floatNum < 1) {
+        const multipliers = [10, 100, 1000, 10000];
+        for (let i = 0; i < multipliers.length; i++) {
+            if (
+                floatNum < 1 / Math.pow(10, i) &&
+                floatNum >= 1 / Math.pow(10, i + 1)
+            ) {
+                return (floatNum * multipliers[i]).toFixed(4);
+            }
+        }
+    } else if (floatNum > 1) {
+        const divisors = [1000, 100, 10, 1];
+        for (let i = 0; i < divisors.length; i++) {
+            if (floatNum < Math.pow(10, i + 1) && floatNum >= Math.pow(10, i)) {
+                return (floatNum / divisors[i]).toFixed(4);
+            }
+        }
     }
-  } else if (floatNum > 1) {
-    if (floatNum < 10000 && floatNum >= 1000) {
-      floatNum = floatNum / 1000
-    } else if (floatNum < 1000 && floatNum >= 100) {
-      floatNum = floatNum / 100
-    } else if (floatNum < 100 && floatNum >= 10) {
-      floatNum = floatNum / 10
-    } else if (floatNum < 10 && floatNum >= 10) {
-      floatNum = floatNum
-    }
-  }
-  return (floatNum.toFixed(4))
+    return floatNum.toFixed(4);
 }
 
+// Calculate the antilog of a value
 function antilog(logValue) {
-  return Math.pow(10, logValue).toPrecision(4);
+    return Math.pow(10, logValue).toPrecision(4);
 }
 
+// Get the characteristic (integer part) of the logarithm of a number
 function getCharacteristic(number) {
-  // Check if the input value is valid
-  let n = Math.abs(number)
-
-  // Calculate the base-10 logarithm
-  const logarithm = Math.log10(n);
-
-  // Extract the characteristic
-  const characteristic = Math.floor(logarithm);
-
-  return characteristic;
+    return Math.floor(Math.log10(Math.abs(number)));
 }
 
-function logarithm(num, power) {
-  if (power == undefined) {
-    return Math.log10(num).toFixed(4)
-  } else {
-    return Math.log10(Math.pow(num, power)).toFixed(4)
-  }
+// Calculate the logarithm of a number, optionally raised to a power
+function logarithm(num, power = 1) {
+    return Math.log10(Math.pow(num, power)).toFixed(4);
 }
 
+// Convert simple numbers to bar form (used for negative numbers)
 function simpleNumbersToBarForm(integer) {
-  if (integer < 0) {
-    if ((integer + 1) > 0 && (integer + 1) < 1) {
-      return (-1) * (integer + 2)
-    } else if ((integer + 2) > 0 && (integer + 2) < 1) {
-      return (-1) * (integer + 4)
-    } else if ((integer + 3) > 0 && (integer + 3) < 1) {
-      return (-1) * (integer + 6)
-    } else if ((integer + 4) > 0 && (integer + 4) < 1) {
-      return (-1) * (integer + 8)
+    if (integer < 0) {
+        for (let i = 1; i <= 4; i++) {
+            if (integer + i > 0 && integer + i < 1) {
+                return -1 * (integer + 2 * i);
+            }
+        }
     }
-  } else {
-    return integer
-  }
+    return integer;
 }
 
-function forGodSake(log, power) {
-  let arr, num, characteristic, tempArr, tempVar1
-  arr = log.split('.')
-  num = arr[1]
-  num = parseFloat(num) / 10 ** (num.length)
-  num = num * power
-  characteristic = parseInt(arr[0]) * power
-  tempArr = (num.toString()).split('.')
-  tempVar1 = parseFloat(tempArr[0])
-  characteristic = tempVar1 + characteristic
-  return ((num.toString()).replace(tempVar1, characteristic))
+// Helper function to process logarithmic calculations
+function processLogarithmicValue(log, power) {
+    const [characteristic, mantissa] = log.split(".");
+    const num = parseFloat(`0.${mantissa}`) * power;
+    const newCharacteristic =
+        parseInt(characteristic) * power + Math.floor(num);
+    return `${newCharacteristic}.${(num % 1).toFixed(4).slice(2)}`;
 }
 
+// Main calculation function
 function calculate() {
-  let multiplyString1 = '';
-  let multiplyString2 = '';
-  let multiplyString3 = '';
-  let multiplyResult = 0
-  let divideString1 = '';
-  let divideString2 = '';
-  let divideString3 = '';
-  let divideResult = 0;
-  let finalResult = 0;
-  let finalString = '';
-  let resultDiv = document.querySelector('.result')
-  document.querySelector('.resultBox').style.height = 'fit-content'
-  let multiplyVar = document.querySelector('#multiply').value
-  let divideVar = document.querySelector('#divide').value
-  let multiplyArr = multiplyVar.split(',')
-  for (let i = 0; i < multiplyArr.length; i++) {
-    let tempArr = multiplyArr[i].split('^')
-    let num = parseFloat(tempArr[0])
-    let power = parseFloat(tempArr[1])
-    let characteristic = getCharacteristic(num).toString()
-    let manualLog = logarithm(decimalConverter(num)).toString()
-    if (i !== 0) {
-      multiplyString1 += `+<p class="log">log</p><p class="round-brac">(</p><span class="number">${num}<sup>${power}</sup></span><p class="round-brac">)</p>`
-      multiplyString2 += `+<p class="round-brac">(</p><span class="power">${power}</span>*<span class="number">${characteristic + manualLog.slice(1)}</span><p class="round-brac">)</p>`
-      multiplyString3 += `+<p class="round-brac">(</p><span class="number">${forGodSake(characteristic + manualLog.slice(1),power)}</span><p class="round-brac">)</p>`
-    } else {
-      multiplyString1 += `<p class="log">log</p><p class="round-brac">(</p><span class="number">${num}<sup>${power}</sup></span><p class="round-brac">)</p>`
-      multiplyString2 += `<p class="round-brac">(</p><span class="power">${power}</span>*<span class="number">${manualLog.replace('0',characteristic)}</span><p class="round-brac">)</p>`
-      multiplyString3 += `<p class="round-brac">(</p><span class="number">${forGodSake(manualLog.replace('0',characteristic),power)}</span><p class="round-brac">)</p>`
-    }
-    multiplyResult = parseFloat(logarithm(num, power)) + multiplyResult
-  }
+    const multiplyInput = document.querySelector("#multiply").value;
+    const divideInput = document.querySelector("#divide").value;
+    const resultDiv = document.querySelector(".result");
+    document.querySelector(".resultBox").style.height = "fit-content";
 
-  let divideArr = divideVar.split(',')
+    // Process multiplication inputs
+    let multiplyResult = 0;
+    let multiplyStrings = { step1: "", step2: "", step3: "" };
+    multiplyInput.split(",").forEach((term, index) => {
+        const [num, power] = term.split("^").map(parseFloat);
+        const characteristic = getCharacteristic(num);
+        const manualLog = logarithm(decimalConverter(num));
+        const logValue = `${characteristic}${manualLog.slice(1)}`;
 
-  for (let i = 0; i < divideArr.length; i++) {
-    let tempArr = divideArr[i].split('^')
-    let num = parseFloat(tempArr[0])
-    let power = parseFloat(tempArr[1])
-    let characteristic = getCharacteristic(num).toString()
-    let manualLog = logarithm(decimalConverter(num)).toString()
-    if (i !== 0) {
-      divideString1 += `+<p class="log">log</p><p class="round-brac">(</p>span class="number">${num}<sup>${power}</sup></span><p class="round-brac">)</p>`
-      divideString2 += `+<p class="round-brac">(</p><span class="power">${power}</span>*<span class="number">${manualLog.replace('0',characteristic)}</span><p class="round-brac">)</p>`
-      divideString3 += `+<p class="round-brac">(</p><span class="number">${forGodSake(manualLog.replace('0',characteristic),power)}</span><p class="round-brac">)</p>`
-    } else {
-      divideString1 += `<p class="log">log</p><p class="round-brac">(</p><span class="number">${num}<sup>${power}</sup></span><p class="round-brac">)</p>`
-      divideString2 += `<p class="round-brac">(</p><span class="power">${power}</span>*<span class="number">${manualLog.replace('0',characteristic)}</span><p class="round-brac">)</p>`
-      divideString3 += `<p class="round-brac">(</p><span class="number">${forGodSake(manualLog.replace('0',characteristic),power)}</span><p class="round-brac">)</p>`
-    }
-    divideResult = parseFloat(logarithm(num, power)) + divideResult
-  }
-  finalResult = multiplyResult - divideResult
-  finalString = `<span class="antilog-text">a.log</span><span class="curly-brac">{</span><p class="sqaure-brac">[</p>${multiplyString1}<p class="sqaure-brac">]</p> - <p class="sqaure-brac">[</p>${divideString1}<p class="sqaure-brac">]</p><span class="curly-brac">}</span></br>
-   = <span class="antilog-text">a.log</span><span class="curly-brac">{</span><p class="sqaure-brac">[</p>${multiplyString2}<p class="sqaure-brac">]</p> - <p class="sqaure-brac">[</p>${divideString2}<p class="sqaure-brac">]</p><span class="curly-brac">}</span></br>
-   = <span class="antilog-text">a.log</span><span class="curly-brac">{</span><p class="sqaure-brac">[</p>${multiplyString3}<p class="sqaure-brac">]</p> - <p class="sqaure-brac">[</p>${divideString3}<p class="sqaure-brac">]</p><span class="curly-brac">}</span></br>
-   = <span class="antilog-text">a.log</span><p class="sqaure-brac">[</p><p class="round-brac">(</p><span class="number">${simpleNumbersToBarForm(multiplyResult).toFixed(4)}</span><p class="round-brac">)</p> - <p class="round-brac">(</p><span class="number">${simpleNumbersToBarForm(divideResult).toFixed(4)}</span><p class="round-brac">)</p><p class="sqaure-brac">]</p></br>
-   = <span class="antilog-text">a.log</span><p class="round-brac">(</p><span class="number">${simpleNumbersToBarForm(finalResult)}</span><p class="round-brac">)</p></br>
-   = <span class="result-value">${antilog(finalResult)}</span>`
-  resultDiv.innerHTML = finalString
+        multiplyStrings.step1 += `${
+            index !== 0 ? "+" : ""
+        }<p class="log">log</p><p class="round-brac">(</p><span class="number">${num}<sup>${power}</sup></span><p class="round-brac">)</p>`;
+        multiplyStrings.step2 += `${
+            index !== 0 ? "+" : ""
+        }<p class="round-brac">(</p><span class="power">${power}</span>*<span class="number">${logValue}</span><p class="round-brac">)</p>`;
+        multiplyStrings.step3 += `${
+            index !== 0 ? "+" : ""
+        }<p class="round-brac">(</p><span class="number">${processLogarithmicValue(
+            logValue,
+            power
+        )}</span><p class="round-brac">)</p>`;
+        multiplyResult += parseFloat(logarithm(num, power));
+    });
+
+    // Process division inputs
+    let divideResult = 0;
+    let divideStrings = { step1: "", step2: "", step3: "" };
+    divideInput.split(",").forEach((term, index) => {
+        const [num, power] = term.split("^").map(parseFloat);
+        const characteristic = getCharacteristic(num);
+        const manualLog = logarithm(decimalConverter(num));
+        const logValue = `${characteristic}${manualLog.slice(1)}`;
+
+        divideStrings.step1 += `${
+            index !== 0 ? "+" : ""
+        }<p class="log">log</p><p class="round-brac">(</p><span class="number">${num}<sup>${power}</sup></span><p class="round-brac">)</p>`;
+        divideStrings.step2 += `${
+            index !== 0 ? "+" : ""
+        }<p class="round-brac">(</p><span class="power">${power}</span>*<span class="number">${logValue}</span><p class="round-brac">)</p>`;
+        divideStrings.step3 += `${
+            index !== 0 ? "+" : ""
+        }<p class="round-brac">(</p><span class="number">${processLogarithmicValue(
+            logValue,
+            power
+        )}</span><p class="round-brac">)</p>`;
+        divideResult += parseFloat(logarithm(num, power));
+    });
+
+    // Calculate final result
+    const finalResult = multiplyResult - divideResult;
+    const finalString = `
+    <span class="antilog-text">a.log</span><span class="curly-brac">{</span><p class="sqaure-brac">[</p>${
+        multiplyStrings.step1
+    }<p class="sqaure-brac">]</p> - <p class="sqaure-brac">[</p>${
+        divideStrings.step1
+    }<p class="sqaure-brac">]</p><span class="curly-brac">}</span></br>
+    = <span class="antilog-text">a.log</span><span class="curly-brac">{</span><p class="sqaure-brac">[</p>${
+        multiplyStrings.step2
+    }<p class="sqaure-brac">]</p> - <p class="sqaure-brac">[</p>${
+        divideStrings.step2
+    }<p class="sqaure-brac">]</p><span class="curly-brac">}</span></br>
+    = <span class="antilog-text">a.log</span><span class="curly-brac">{</span><p class="sqaure-brac">[</p>${
+        multiplyStrings.step3
+    }<p class="sqaure-brac">]</p> - <p class="sqaure-brac">[</p>${
+        divideStrings.step3
+    }<p class="sqaure-brac">]</p><span class="curly-brac">}</span></br>
+    = <span class="antilog-text">a.log</span><p class="sqaure-brac">[</p><p class="round-brac">(</p><span class="number">${simpleNumbersToBarForm(
+        multiplyResult
+    ).toFixed(
+        4
+    )}</span><p class="round-brac">)</p> - <p class="round-brac">(</p><span class="number">${simpleNumbersToBarForm(
+        divideResult
+    ).toFixed(
+        4
+    )}</span><p class="round-brac">)</p><p class="sqaure-brac">]</p></br>
+    = <span class="antilog-text">a.log</span><p class="round-brac">(</p><span class="number">${simpleNumbersToBarForm(
+        finalResult
+    )}</span><p class="round-brac">)</p></br>
+    = <span class="result-value">${antilog(finalResult)}</span>`;
+    resultDiv.innerHTML = finalString;
 }
 
+// Set body size to viewport dimensions
 function setBodySizeToViewport() {
-  var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-  var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-
-  document.body.style.height = windowHeight + "px";
-  document.body.style.width = windowWidth + "px";
+    const windowHeight =
+        window.innerHeight ||
+        document.documentElement.clientHeight ||
+        document.body.clientHeight;
+    const windowWidth =
+        window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.body.clientWidth;
+    document.body.style.height = `${windowHeight}px`;
+    document.body.style.width = `${windowWidth}px`;
 }
 
-// Call the function initially to set the body size to the viewport
+// Initialize
 setBodySizeToViewport();
